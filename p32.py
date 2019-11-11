4148bb1f4f50116df3c0cf081d4c0e248e8e1e85
afarica <57631090+afarica@users.noreply.github.com>
2019-11-12 04:32:13 +0600

    Update p32.py

--- p32.py
+++ p32.py
@@ -1,3 +1,7 @@
+# Given a string that may or may not contain a letter of interest. Print the index
+# location of the first and last appearance of f. If the letter f occurs only once,
+# then output its index. If the letter f does not occur, then do not print anything.
+Don't use loops in this task.
 a=input("Enter your string:")
 b=a.count("f")
 if b!=0:
